67cbf9babc3946a6561221e826c963dbbf2f6d52
Dennis Lee <dennisl88@berkeley.edu>
2017-11-22 15:22:59 -0500

    updates from Keiran

--- rpd_interfaces/generals/simulator.py
+++ rpd_interfaces/generals/simulator.py
@@ -3,6 +3,14 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 import queue
+from random import randint
+import json
+import os
+
+import matplotlib
+from PIL import Image, ImageDraw, ImageFont
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # Terrain
 _EMPTY = 0
@@ -10,187 +18,234 @@ _GENERAL = 1
 _CITY = 2
 _MOUNTAIN = 3
 _FOG = 4
+_FOGGY_TERRAIN = 5
 _FOGGY_CITY = 6
 _FOGGY_MOUNTAIN = 7
 _NEUTRAL = 0
 
-_MIN_CITY_DIST = 5
-_MIN_GENERAL_DIST = 10
-
 _CITY_MAX_ARMY = 40
 
+_MAP_SIZE = 18
 
-def land_dt(player, state, next_state):
+_TURN_LIMIT = 1000
+
+
+def land_dt(player, state, next_state, opponent_land_count):
     if state is None:
         return np.sum(next_state[1] == player.id_no)
     return np.sum(next_state[1] == player.id_no) - np.sum(state[1] == player.id_no)
 
+def win_loss(player, state, next_state, opponent_land_count):
+    if not opponent_land_count:
+        return 1
+    else:
+        return 0
+
+class RejectingStack:
+    def __init__(self):
+        self.stack = []
+        self.is_open = False
+
+    def put(self, item):
+        if self.is_open:
+            self.stack.append(item)
+
+    def get(self):
+        return self.stack.pop()
+
+    def open(self):
+        self.is_open = True
+
+    def close(self):
+        self.is_open = False
+
+    def clear(self):
+        self.stack = []
+
+    def empty(self):
+        return not len(self.stack)
+
+
 class Player(object):
-    def __init__(self, id_no, general_loc, reward_fn=land_dt):
+    def __init__(self, id_no, general_loc, reward_fn=win_loss):
         self.id_no = id_no
         self.actions = queue.Queue()
         self.outputs = queue.Queue()
+        self.rewards = queue.Queue()
         self.last_state = None
         self.reward_fn = reward_fn
-        self.general_loc = general_loc.tolist()
+        self.general_loc = general_loc
 
     def set_action(self, action):
         self.actions.put(action)
 
     def get_action(self):
-        return self.actions.get(block=False)  # previously blocking
+        return self.actions.get()
 
     def set_output(self, obs):
         self.outputs.put(obs)
 
     def get_output(self):
-        """
-        TODO: do we want to set `self.last_state` here? All this blocking stuff is confusing me.
-        Are we trying to require that players make moves each half-turn? and/or parallelism?
-        If not, a player could make a ton of actions and mess up the output queue...?
-        But if we are, there's like a mutual blocking thing going on with these two queues right now...?
-        """
         new_state = self.outputs.get()
         self.last_state = new_state
         return new_state
 
 class Map(object):
-    def __init__(self, shape, player_count):
-        self.width = shape[0]
-        self.height = shape[1]
+    def __init__(self):
+        self.width = _MAP_SIZE
+        self.height = _MAP_SIZE
 
-        self.terrain = np.zeros(shape)
-        self.armies = np.zeros(shape)
-        self.owner = np.zeros(shape)
+        self.terrain = np.zeros((_MAP_SIZE, _MAP_SIZE))
+        self.armies = np.zeros((_MAP_SIZE, _MAP_SIZE))
+        self.owner = np.zeros((_MAP_SIZE, _MAP_SIZE))
         self.turn_count = 1
         self.grid = np.vstack(np.mgrid[:self.width, :self.height].T)
+        self.num_players = 0
+        self.players = {}
+        self.cities = []
+        self.generals = []
+        self.undo_queue = RejectingStack()
 
-        for _ in range(self.width * self.height // 3):
-            self.terrain[np.random.randint(self.width), np.random.randint(self.height)] = _MOUNTAIN
+    def add_army(self, pos, player, army):
+        self.owner[pos[0], pos[1]] = player
+        self.armies[pos[0], pos[1]] = army
 
-        self.cities = np.hstack((np.random.randint(self.width, size = (2 * player_count, 1)),
-                                 np.random.randint(self.height, size = (2 * player_count, 1))))
+    def add_mountain(self, pos):
+        self.terrain[pos[0], pos[1]] = _MOUNTAIN
 
-        for x, y in self.cities:
-            self.terrain[x, y] = _CITY
-            self.armies[x, y] = _CITY_MAX_ARMY
+    def add_city(self, pos, army):
+        self.terrain[pos[0], pos[1]] = _CITY
+        self.armies[pos[0], pos[1]] = army
+        self.cities.append(pos)
 
-        self.generals = np.hstack((np.random.randint(self.width, size = (player_count, 1)),
-                                   np.random.randint(self.height, size = (player_count, 1))))
+    def add_general(self, pos, player_id=-1):
+        self.num_players += 1
+        if player_id == -1:
+            player_id = self.num_players
+        self.terrain[pos[0], pos[1]] = _GENERAL
+        self.owner[pos[0], pos[1]] = player_id
+        self.armies[pos[0], pos[1]] = 1
+        self.players[player_id] = Player(player_id, pos)
+        self.generals.append(pos)
 
-        for i, loc in enumerate(self.generals):
-            x, y = loc
-            self.terrain[x, y] = _GENERAL
-            self.owner[x, y] = i + 1
+    def save(self):
+        self.undo_queue.open()
+        self.undo_queue.clear()
 
-        self.players = {i: Player(i, self.generals[i-1]) for i in range(1, player_count + 1)}
-        self.remaining_players = player_count
-        """
-        self.cities = np.zeros((player_count * 2, 2))
-        self.generals = np.zeros((player_count, 2))
-        for i in range(player_count):
-            self.cities[2*i] = np.array([np.random.randint(self.width), np.random.randint(self.height)])
-        """
+    def load(self):
+        while not self.undo_queue.empty():
+            self.undo_queue.get()()
 
-    def _run_turn(self, verbose=False):
-        """Simulate a single turn of the game. Technically this is a single "half-turn" of the game.
-
-        A turn consists of the following actions:
-        -----------------------------------------
-          - execute the next action in each player's queue
-          - generate observations for each player
-          - spawn units on owned squares, if applicable
-        """
+    def update(self, fast_mode=False):
         for player in self.players.values():
-            start_location, end_location, is_half = player.get_action()
-            self._execute_action(player, start_location, end_location, is_half)
-        for player in self.players.values():
-            self._generate_obs(player)
+            if not player.actions.empty():
+                start_location, end_location = player.get_action()
+                self._execute_action(player, start_location, end_location)
         self._spawn()
+        if not fast_mode:
+            for player in self.players.values():
+                self._generate_obs(player)
         self.turn_count += 1
-        if verbose:
-            self.print_state()
+        def undo_turn_count():
+            self.turn_count -= 1
+        self.undo_queue.put(undo_turn_count)
 
-    def _update(self):
-        """Game loop."""
-        self._run_turn()
-        if self.remaining_players > 1:
-            self._run_turn()
-
-    def _execute_action(self, player, start_location, end_location, is_half=False):
-        """Update maps to account for player PLAYER making an action from START_LOCATION to END_LOCATION."""
-        if end_location is None:
-            return
+    def _execute_action(self, player, start_location, end_location):
         s_x, s_y = start_location
         e_x, e_y = end_location
         if self.owner[s_x, s_y] == player.id_no \
-                and self.armies[s_x, s_y] > 1 \
-                and np.abs(s_x - e_x) + np.abs(s_y - e_y) == 1:
+            and self.armies[s_x, s_y] > 1 \
+            and np.abs(s_x - e_x) + np.abs(s_y - e_y) == 1: 
             moving = self.armies[s_x, s_y] - 1
-            if is_half:
-                moving //= 2  # round down or up? currently not sure
             self.armies[s_x, s_y] = 1
+            def undo_take_army():
+                self.armies[s_x, s_y] += moving
+            self.undo_queue.put(undo_take_army)
             if self.owner[e_x, e_y] == player.id_no:
                 self.armies[e_x, e_y] += moving
+                def undo_move_army_to_ally():
+                    self.armies[e_x, e_y] -= moving
+                self.undo_queue.put(undo_move_army_to_ally)
             else:
                 self.armies[e_x, e_y] -= moving
-                if self.armies[e_x, e_y] < 0: # If target square has value 0, does it go neutral?
+                def undo_move_army_to_enemy():
+                    self.armies[e_x, e_y] += moving
+                self.undo_queue.put(undo_move_army_to_enemy)
+                if self.armies[e_x, e_y] < 0:
                     if self.terrain[e_x, e_y] == _GENERAL:
                         defeated = self.owner[e_x, e_y]
                         defeated_map = (self.owner == defeated)
                         diff = defeated - player.id_no
                         self.owner -= defeated_map * diff
                         self.remaining_players -= 1
+                        def undo_kill():
+                            self.remaining_players += 1
+                            self.owner += defeated_map * diff
+                        self.undo_queue.put(undo_kill)
                     self.armies[e_x, e_y] *= -1
+                    prev_owner = self.owner[e_x, e_y]
                     self.owner[e_x, e_y] = player.id_no
+                    def undo_ownership_change():
+                        self.owner[e_x, e_y] = prev_owner
+                        self.armies[e_x, e_y] *= -1
+                    self.undo_queue.put(undo_ownership_change)
         else:
             print("Invalid action {} {}".format(start_location, end_location))
 
     def _generate_obs(self, player):
-        """Generates an observation, as a (state, reward, done) tuple, for the given player.
-        This observation will be saved in the player's internal queue.
-        """
-        new_state = self.generate_state(player)
-        reward = player.reward_fn(player, player.last_state, new_state)
-        done = self.owner[player.general_loc] == player.id_no
-        player.set_output((new_state, reward, done))
-
-    def _spawn(self):
-        """Spawn units wherever units should be spawned."""
-        if self.turn_count % 2 == 0:  # units will not be spawned on half-turns
-            for x, y in self.cities:
-                if self.owner[x, y] != _NEUTRAL or self.armies[x, y] < _CITY_MAX_ARMY:
-                    self.armies[x, y] += 1
-            for x, y in self.generals:
-                self.armies[x, y] += 1
-
-            if self.turn_count % 50 == 0:
-                player_owned = (self.owner > 0)
-                self.armies += player_owned
-
-    def generate_state(self, player):
-        """Generate state for PLAYER as a (terrain, armies, ownership) tuple."""
+        if self.turn_count >= _TURN_LIMIT:
+            player.set_output(([], 0, True))
         player_owned = np.transpose((self.owner == player.id_no).nonzero())
-        distances = np.min(cdist(self.grid, player_owned, 'cityblock'), axis=1).reshape(self.height, self.width).T
-        seen = distances <= 1
+        distances = np.min(cdist(self.grid, player_owned, 'euclidean'), axis=1).reshape(self.height, self.width).T
+        seen = distances <= 1.5
         fog = 1 - seen
         visible_terrain = self.terrain * seen + fog * _FOG + self.terrain * (self.terrain != _GENERAL) * fog
+        visible_terrain[visible_terrain == _FOGGY_CITY] = _FOGGY_TERRAIN
+        visible_terrain[visible_terrain == _FOGGY_MOUNTAIN] = _FOGGY_TERRAIN
         visible_armies = self.armies * seen
         visible_owner = self.owner * seen
-        return visible_terrain, visible_armies, visible_owner
+        opponent_owned = self.owner = self.other_player(player.id_no)
+        opponent_land_count = np.sum(opponent_owned)
+        opponent_army_count = np.sum(self.armies[opponent_owned])
+        new_state = (visible_terrain, visible_armies, visible_owner, opponent_land_count, opponent_army_count)
+        reward = player.reward_fn(player, player.last_state, new_state, opponent_land_count)
+        done = self.owner[player.general_loc] != player.id_no
+        player.set_output((new_state, reward, done))
 
-    def action(self, player_id, start_location, end_location, is_half=False):
-        """Have the player with PLAYER_ID input an action from START_LOCATION to END_LOCATION.
-        The action will not actually be executed at this time;
-        rather, it will be added to a queue of actions to be taken in the future.
 
-        START_LOCATION and END_LOCATION are currently formatted as shape (2,) points.
-        """
-        if player_id in self.players:
-            self.players[player_id].set_action((start_location, end_location, is_half))
-            return self.players[player_id].get_output()
-        print("Invalid player %d", player_id)
-        raise
+    def _spawn(self):
+        for x,y in self.cities:
+            if self.owner[x,y] != _NEUTRAL or self.armies[x,y] < _CITY_MAX_ARMY:
+                self.armies[x,y] += 1
+        for x,y in self.generals:
+            self.armies[x,y] += 1
+
+        if self.turn_count % 25 == 0:
+            player_owned = (self.owner > 0)
+            self.armies += player_owned
+        def undo_spawn():
+            for x,y in self.cities:
+                if self.owner[x,y] != _NEUTRAL or self.armies[x,y] < _CITY_MAX_ARMY:
+                    self.armies[x,y] -= 1
+            for x,y in self.generals:
+                self.armies[x,y] -= 1
+
+            if self.turn_count % 25 == 0:
+                player_owned = (self.owner > 0)
+                self.armies -= player_owned
+        self.undo_queue.put(undo_spawn)
+
+    def action(self, player_id, start_location, end_location): # is_half, player_id
+        if player_id in self.players and self.owner[start_location] == player_id:
+            if 0 <= end_location[0] < _MAP_SIZE and 0 <= end_location[1] < _MAP_SIZE and self.armies[start_location] > 1:
+                self.players[player_id].set_action((start_location, end_location))
+            return
+        raise ValueError
+
+    def other_player(self, player_id):
+        if player_id == 1:
+            return 2
+        return 1
 
     def __str__(self):
         return "{} \n {} \n {} \n".format(self.terrain, self.armies, self.owner)
@@ -199,3 +254,83 @@ class Map(object):
         print(self.terrain)
         print(self.owner)
         print(self.armies)
+
+class GeneralsEnv:
+    def __init__(self, root_dir):
+        listdir = os.listdir(root_dir)
+        self.replays = [root_dir + file for file in listdir if file.endswith('.gioreplay')]
+
+    def reset(self):
+        """Sets the map using a random replay"""
+        self.map = self._get_random_map()
+        self.map.update()
+        return [x.get_output() for x in self.map.players.values()]
+
+    def step(self, action1, action2):
+        """Takes two tuples of (start_location, end_location).
+        Action 1 is for player 1, 2 is for player 2
+        Returns: (state1, reward1, dead1), (state2, reward2, dead2)
+        """
+        self.map.action(1, action1[0], self._get_movement_for_action(action1[0], action1[1]))
+        self.map.action(2, action2[0], self._get_movement_for_action(action2[0], action2[1]))
+        self.map.update()
+        return [x.get_output() for x in self.map.players.values()]
+
+    def _get_movement_for_action(self, initial, direction):
+        """Maps 0, 1, 2, 3 to up, down, left, right"""
+        dirs = {0: (0, -1), 1: (0, 1), 2: (-1, 0), 3: (1, 0)}
+        return initial[0] + dirs[direction][0], initial[1] + dirs[direction][1]
+
+    def _get_random_map(self):
+        while True:
+            index = randint(0, len(self.replays) - 1)
+            replay = json.load(open(self.replays[index]))
+            if len(replay['usernames']) == 2 and replay['mapWidth'] == 18 and replay['mapHeight'] == 18:
+                break
+        m = Map()
+        for mountain in replay['mountains']:
+            m.add_mountain(self._flat_to_2d(mountain))
+        for i in range(len(replay['cities'])):
+            m.add_city(self._flat_to_2d(replay['cities'][i]), replay['cityArmies'][i])
+        for general in replay['generals']:
+            m.add_general(self._flat_to_2d(general))
+        return m
+
+    def _flat_to_2d(self, index):
+        return index // _MAP_SIZE, index % _MAP_SIZE
+
+    def get_image_of_state(self, state):
+        terrain, armies, owner = state
+        size = 400
+        step = size / _MAP_SIZE
+        image = Image.new('RGBA', (size, size), (255, 255, 255, 255))
+        d = ImageDraw.Draw(image)
+        colors = [
+            (255, 255, 255, 255), # Empty - white
+            (), # General - no color
+            (0, 136, 34, 255), # City - Green
+            (128, 64, 0, 255), # Mountain - Brown
+            (32, 32, 32, 255), # Fog - Dark Grey
+            (124, 124, 124, 255), # Terrain Fog - Grey
+            (0, 0, 255, 255), # Blue player
+            (255, 0, 0, 255) # Red player
+        ]
+        dir = os.path.dirname(__file__)
+        font = ImageFont.truetype(os.path.join(dir, 'FreeMono.ttf'), 40)
+        for i in range(_MAP_SIZE):
+            for j in range(_MAP_SIZE):
+                if owner[i, j] > 0:
+                    d.rectangle((i * step, j * step, i * step + step, j * step + step), fill=colors[int(owner[i, j]) + 5])
+                else:
+                    d.rectangle((i * step, j * step, i * step + step, j * step + step), fill=colors[int(terrain[i, j])])
+                if armies[i, j] > 0:
+                    general = ''
+                    if terrain[i, j] == _GENERAL:
+                        general = '*'
+                    d.text((i * step + step//4, j * step + step//4), str(int(armies[i, j])) + general, fill=colors[0])
+                if terrain[i, j] == _MOUNTAIN:
+                    d.text((i * step + step//4, j * step + step//4), '^^', fill=colors[0])
+                if terrain[i, j] == _FOGGY_TERRAIN:
+                    d.text((i * step + step//4, j * step + step//4), '??', fill=colors[0])
+        del d
+        return image
